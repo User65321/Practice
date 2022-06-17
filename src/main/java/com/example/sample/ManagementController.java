7a22f9579552f5435dc5072d97bea1efcfa099f8
木村　駿平 <>
2022-06-17 17:52:15 +0900

    起動エラー発生。

---
+++ src/main/java/com/example/sample/ManagementController.java
@@ -0,0 +1,50 @@
+package com.example.sample;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import service.bookService;
+
+@Controller
+public class ManagementController {
+
+	@Autowired
+	public bookService manager;
+	
+	
+	//ここから登録
+	@GetMapping("/")
+	public String bookRegister() {
+		return "bookRegister";
+	}
+	
+	@PostMapping("/")
+	public String bookRegister(ModelMap modelMap,@RequestParam("Number") Integer Number,@RequestParam("Name")String Name) {
+		manager.Register(modelMap, Number, Name);
+		return "bookRegister";
+	}
+	
+	//ここから検索
+		@GetMapping("bookSearch")
+		public String bookSearch() {
+			return "bookSearch";
+		}
+		@PostMapping("bookSearch")
+		public String bookSearch(ModelMap modelMap,@RequestParam("Number")Integer Number) {
+			manager.Search(modelMap, Number);
+			return "bookSearch";
+		}
+		
+		//ここから一覧
+		@GetMapping("bookList")
+		public String bookList(ModelMap modelMap) {
+			manager.List(modelMap);
+			return "bookList";
+		}
+	
+	
+}
